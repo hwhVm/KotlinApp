80f70482fd7b902341d05a2c664218181292c7ff
beini <874140704@qq.com>
2017-06-03 14:27:39 +0800

    create repository

---
+++ app/src/main/java/beini/com/kotlinapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package beini.com.kotlinapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
